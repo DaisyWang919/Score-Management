7bc4fa1ce8f652325acdb66ce723af2c1b0239eb
DaisyWang <zijun.wang@emory.edu>
2023-06-09 11:38:59 +0800

    Backend finished

---
+++ Back/src/main/java/org/example/pojo/Student.java
@@ -0,0 +1,15 @@
+package org.example.pojo;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private long id;
+    private String number;
+    private String name;
+    private int age;
+    private int cal;
+    private int econ;
+    private int cs;
+
+}
